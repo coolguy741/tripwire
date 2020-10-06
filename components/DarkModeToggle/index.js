53de9fa32a6c2aba639bc49f7659e731e3ad9885
coolguy741 <forest915820@gmail.com>
2020-10-06 15:21:55 -0600

    Clean up toggle

--- components/DarkModeToggle/index.js
+++ components/DarkModeToggle/index.js
@@ -1,25 +1,23 @@
-import React, { useState } from "react";
-import { useContext } from "react";
+import React, { useContext } from "react";
 import Toggle from "react-toggle";
 import { Context } from "../../context/context";
-import styled from "styled-components";
 import Moon from "./Moon";
 import Sun from "./Sun";
+import styled from "styled-components";
+
+const DarkMode = styled.div`
+    position: absolute;
+    top: 22px;
+    right: 20px;
+`;
 
 const DarkModeToggle = (props) => {
     const { darkMode } = useContext(Context);
-
-    const DarkMode = styled.div`
-        position: absolute;
-        top: 22px;
-        right: 20px;
-    `;
 
     return (
         <DarkMode>
             <Toggle
                 defaultChecked={darkMode.value}
-                aria-label="No label tag"
                 className="reactToggle"
                 onChange={darkMode.toggle}
                 icons={{
